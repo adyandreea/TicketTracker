02a1771052ff07b807fec76d4721d2bc14606318
Andritchii Andreea <adyandreea48@gmail.com>
2026-02-12 22:56:31 +0200

    Implement i18n setup

--- frontend/src/routes/AppRouter.js
+++ frontend/src/routes/AppRouter.js
@@ -7,55 +7,58 @@ import RegisterPage from "../features/pages/RegisterPage.js";
 import EditUserPage from "../features/pages/EditUserPage.js";
 import ProtectedRoute from "../utils/ProtectedRoute";
 import LogoutHandler from "../utils/LogoutHandler";
+import { LanguageProvider } from "../i18n/LanguageContext";
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <DashboardPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/projects"
-          element={
-            <ProtectedRoute>
-              <ProjectPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/boards"
-          element={
-            <ProtectedRoute>
-              <BoardPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <ProtectedRoute>
-              <RegisterPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/edit-user"
-          element={
-            <ProtectedRoute>
-              <EditUserPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/logout" element={<LogoutHandler />} />
-      </Routes>
-    </BrowserRouter>
+    <LanguageProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/projects"
+            element={
+              <ProtectedRoute>
+                <ProjectPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/boards"
+            element={
+              <ProtectedRoute>
+                <BoardPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <ProtectedRoute>
+                <RegisterPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/edit-user"
+            element={
+              <ProtectedRoute>
+                <EditUserPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/logout" element={<LogoutHandler />} />
+        </Routes>
+      </BrowserRouter>
+    </LanguageProvider>
   );
 };
 
