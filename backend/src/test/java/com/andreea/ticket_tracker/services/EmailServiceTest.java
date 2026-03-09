567c7c2aa9c25107962ad2152d0821d4da896929
Andritchii Andreea <adyandreea48@gmail.com>
2026-03-09 23:41:16 +0200

    Add unit tests for email notification

---
+++ backend/src/test/java/com/andreea/ticket_tracker/services/EmailServiceTest.java
@@ -0,0 +1,51 @@
+package com.andreea.ticket_tracker.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class EmailServiceTest {
+
+    @Mock
+    private JavaMailSender mailSender;
+
+    @InjectMocks
+    private EmailService emailService;
+
+    @BeforeEach
+    void setUp() {
+        ReflectionTestUtils.setField(emailService, "senderEmail", "noreply@test.com");
+    }
+
+    @Test
+    void testSendSimpleEmail() {
+        String to = "destinatar@gmail.com";
+        String subject = "subject";
+        String body = "body";
+
+        emailService.sendSimpleEmail(to, subject, body);
+
+        ArgumentCaptor<SimpleMailMessage> messageCaptor = ArgumentCaptor.forClass(SimpleMailMessage.class);
+
+        verify(mailSender, times(1)).send(messageCaptor.capture());
+
+        SimpleMailMessage sentMessage = messageCaptor.getValue();
+
+        assertEquals("noreply@test.com", sentMessage.getFrom());
+        assertEquals(to, sentMessage.getTo()[0]);
+        assertEquals(subject, sentMessage.getSubject());
+        assertEquals(body, sentMessage.getText());
+    }
+}
